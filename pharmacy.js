fc1b28a9365a1f2806d525b7d90c39c1f5a46885
Promtech <uwagboepromise5@gmail.com>
2022-05-17 12:21:04 +0100

    Added a js file to run some funtionalities

---
+++ pharmacy.js
@@ -0,0 +1,37 @@
+var hamburger = document.querySelector(".hamburger")
+var navbar = document.querySelector(".navbar")
+var remove = document.querySelector(".mobile-nav")
+
+
+
+hamburger.addEventListener("click", function(){
+    console.log("open")
+
+    if(hamburger.classList.contains("open")){
+        hamburger.classList.remove("open");
+        
+    }else{
+        hamburger.classList.add("open");
+    }
+
+    if(remove.classList.contains("first")){
+        remove.classList.remove("first")
+    }else{
+        remove.classList.add("first")
+    }
+
+
+    if(remove.classList.contains("mobile-open")){
+        remove.classList.remove("mobile-open")
+    }else{
+        remove.classList.add("mobile-open")
+    }
+
+    if(navbar.classList.contains("mini")){
+        navbar.classList.remove("mini")
+    }else{
+        navbar.classList.add("mini")
+    }
+
+   
+})
